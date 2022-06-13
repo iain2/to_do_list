eacd089abcc4f1940a54755e18137fb4ad63b0b3
Iain Hay <iainhay@Iains-MacBook-Air.local>
2022-06-13 20:07:18 +0100

    mvp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    'Buy shopping',
+    'Clean bathroom',
+    'Car\'s MOT'
+  ])
+
+  const [newTask, setNewTask] = useState('');
+
+  const itemNodes = tasks.map((task, index) => {
+    return (
+      <li key={index}><span>{task}</span></li>
+    )
+  })
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    const copyTasks = [...tasks];
+    copyTasks.push(newTask);
+    setTasks(copyTasks);
+    setNewTask('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>ToDo List</h1>
+
+        <form onSubmit={saveNewTask}>
+
+          <input id='new-task' type='text' value={newTask} onChange={handleTaskInput} />
+          <input type='radio' id='high' name='high' value='high'></input>
+          <input type='radio' id='low' name='low' value='low'></input>
+          <input type='submit' value='save new task' />
+        </form>
+
+        <ul>
+          {itemNodes}
+        </ul>
+      </div>
+    </>
   );
 }
 
